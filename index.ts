b2ad10cd3c65cde94c712c3ddbb67faf7fc9ba07
Anwesh43 <anweshthecool0@gmail.com>
2021-03-22 13:43:16 +0530

    adding Animator class

--- index.ts
+++ index.ts
@@ -36,3 +36,22 @@ class Stage {
 
 }
 
+class Animator {
+
+    animated : boolean = false 
+    interval : number 
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false 
+            clearInterval(this.interval)
+        }
+    }
+}
